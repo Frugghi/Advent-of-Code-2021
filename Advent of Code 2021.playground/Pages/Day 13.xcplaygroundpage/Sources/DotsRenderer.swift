05fe548f99badf672dbf517751d478caf5014170
Tommaso Madonia <tommaso@madonia.me>
2021-12-13 17:27:20 +0000

    Use Vision framework to return a string in day 13

--- Advent of Code 2021.playground/Pages/Day 13.xcplaygroundpage/Sources/DotsRenderer.swift
+++ Advent of Code 2021.playground/Pages/Day 13.xcplaygroundpage/Sources/DotsRenderer.swift
@@ -1,40 +1,26 @@
 import Foundation
+import Vision
 import UIKit
 
-public struct DotsRenderer: CustomStringConvertible, CustomPlaygroundDisplayConvertible {
+public final class DotsRenderer: CustomStringConvertible, CustomPlaygroundDisplayConvertible {
     private let points: Set<Point>
     private let maxX: Int
     private let maxY: Int
 
-    public var description: String {
-        var matrix = Array(repeating: Array(repeating: " ", count: maxX + 1), count: maxY + 1)
-        for point in points {
-            matrix[point.y][point.x] = "█"
+    public private(set) lazy var description: String = {
+        if let ocrString = ocr() {
+            return ocrString
+        } else {
+            var matrix = Array(repeating: Array(repeating: " ", count: maxX + 1), count: maxY + 1)
+            for point in points {
+                matrix[point.y][point.x] = "█"
+            }
+            return matrix.map { $0.joined(separator: "") }.joined(separator: "\n")
         }
-        return matrix.map { $0.joined(separator: "") }.joined(separator: "\n")
-    }
+    }()
 
     public var playgroundDescription: Any {
-        let dotSize = CGSize(width: 8, height: 8)
-        let spacing: CGFloat = 4
-        let horizontalInset: CGFloat = 12
-        let verticalInset: CGFloat = 12
-        let width = CGFloat(maxX + 1) * dotSize.width + CGFloat(maxX) * spacing + horizontalInset * 2
-        let height = CGFloat(maxY + 1) * dotSize.height + CGFloat(maxY) * spacing + verticalInset * 2
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
-        return renderer.image { context in
-            UIColor.white.setFill()
-            for point in points {
-                let rect = CGRect(
-                    origin: CGPoint(
-                        x: horizontalInset + CGFloat(point.x) * dotSize.width + CGFloat(point.x) * spacing,
-                        y: verticalInset + CGFloat(point.y) * dotSize.height + CGFloat(point.y) * spacing
-                    ),
-                    size: dotSize
-                )
-                context.cgContext.fillEllipse(in: rect)
-            }
-        }
+        renderImage(usingCircles: true)
     }
 
     public init<S>(dots: S) where S: Sequence, S.Element == Point {
@@ -51,4 +37,53 @@ public struct DotsRenderer: CustomStringConvertible, CustomPlaygroundDisplayConv
         self.maxY = maxY
     }
 
+    private func ocr() -> String? {
+        guard let cgImage = renderImage(usingCircles: false).cgImage else { return nil }
+
+        var result: String?
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+        let request = VNRecognizeTextRequest { request, _ in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                return
+            }
+
+            let recognizedStrings: [String] = observations.compactMap { observation in
+                observation.topCandidates(1).first?.string.trimmingCharacters(in: .whitespaces)
+            }
+
+            if !recognizedStrings.isEmpty {
+                result = recognizedStrings.joined(separator: "")
+            }
+        }
+        request.recognitionLevel = .fast
+
+        do {
+            try requestHandler.perform([request])
+        } catch {}
+
+        return result
+    }
+
+    private func renderImage(usingCircles circles: Bool) -> UIImage {
+        let dotSize = CGSize(width: 8, height: 8)
+        let spacing: CGFloat = circles ? 4 : 0
+        let horizontalInset: CGFloat = 12
+        let verticalInset: CGFloat = 12
+        let width = CGFloat(maxX + 1) * dotSize.width + CGFloat(maxX) * spacing + horizontalInset * 2
+        let height = CGFloat(maxY + 1) * dotSize.height + CGFloat(maxY) * spacing + verticalInset * 2
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
+        return renderer.image { context in
+            UIColor.white.setFill()
+            for point in points {
+                let rect = CGRect(
+                    origin: CGPoint(
+                        x: horizontalInset + CGFloat(point.x) * dotSize.width + CGFloat(point.x) * spacing,
+                        y: verticalInset + CGFloat(point.y) * dotSize.height + CGFloat(point.y) * spacing
+                    ),
+                    size: dotSize
+                )
+                circles ? context.cgContext.fillEllipse(in: rect) : context.cgContext.fill(rect)
+            }
+        }
+    }
 }
